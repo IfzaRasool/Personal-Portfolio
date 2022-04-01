e61da70276fe2fa16f65e809c7bcd9df5398cd32
IfzaRasool <ifzarasoolarain@gmail.com>
2022-04-01 12:37:05 +0300

    Add <arkup file -js file -Add css file -Add project images -linters

---
+++ js/localStorage.js
@@ -0,0 +1,34 @@
+const contactForm = document.querySelector('#contact-form');
+const fullName = document.querySelector('#name');
+const emailAddress = document.querySelector('#email');
+const message = document.querySelector('#message');
+const reset = document.getElementById('reset');
+
+fullName.value = '';
+emailAddress.value = '';
+message.value = '';
+
+function dataStorage() {
+  const user = {
+    userFullName: fullName.value,
+    userEmail: emailAddress.value,
+    userMessage: message.value,
+  };
+
+  localStorage.setItem('userData', JSON.stringify(user));
+}
+
+contactForm.addEventListener('focusout', dataStorage);
+
+const userDataObject = JSON.parse(localStorage.getItem('userData'));
+
+fullName.value = userDataObject.userFullName;
+emailAddress.value = userDataObject.userEmail;
+message.value = userDataObject.userMessage;
+
+reset.addEventListener('click', () => {
+  localStorage.removeItem('userData');
+  fullName.value = '';
+  emailAddress.value = '';
+  message.value = '';
+});
